4f74699385e8c8a65e88b82940f6d9e07f46dbbc
Ian Lavery <ian@picovoice.ai>
2026-06-02 12:43:38 -0700

    fix

--- .github/actions/node-sea/index.js
+++ .github/actions/node-sea/index.js
@@ -1,6 +1,10 @@
 const path = require('path');
+const { createRequire } = require('module');
+
 const addonPath = path.join(__dirname, '../../../lib/node/windows/arm64/pv_recorder.node');    
-const addon = require(addonPath);
+const requireFromDisk = createRequire(process.execPath);
+
+const addon = requireFromDisk(addonPath);
 const devices = addon.get_available_devices();
 console.log("Success! Found", devices.length, "audio device(s):");
 devices.forEach((d, i) => console.log(`  [${i}] ${d}`));
