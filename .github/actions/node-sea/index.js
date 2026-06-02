89cbd75805630d6a8790fd68979c08c357c42cad
Ian Lavery <ian@picovoice.ai>
2026-06-02 12:49:40 -0700

    better print

--- .github/actions/node-sea/index.js
+++ .github/actions/node-sea/index.js
@@ -4,7 +4,12 @@ const { createRequire } = require('module');
 const addonPath = path.join(__dirname, '../../../lib/node/windows/arm64/pv_recorder.node');    
 const requireFromDisk = createRequire(process.execPath);
 
-const addon = requireFromDisk(addonPath);
-const devices = addon.get_available_devices();
-console.log("Success! Found", devices.length, "audio device(s):");
-devices.forEach((d, i) => console.log(`  [${i}] ${d}`));
+try {
+    const addon = requireFromDisk(addonPath);
+    const devices = addon.get_available_devices();
+    console.log("Found", devices.length, "audio device(s):");
+    devices.forEach((d, i) => console.log(`  [${i}] ${d}`));
+} catch (e) {
+    console.error("Failed to load addon:", e);
+    process.exit(1);
+}
